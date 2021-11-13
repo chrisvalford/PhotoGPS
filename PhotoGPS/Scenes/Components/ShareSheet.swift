67e0889f7902a601b59546e87d3030732aa6f92f
Christopher Alford <chrisvalford@me.com>
2021-11-13 12:51:33 +0100

    PGPS008: Extracted Share into new view, added "wrapped" ActivityViewController, and fixed navigation

---
+++ PhotoGPS/Scenes/Components/ShareSheet.swift
@@ -0,0 +1,82 @@
+//
+//  ShareSheet.swift
+//  PhotoGPS
+//
+//  Created by Christopher Alford on 13/11/21.
+//
+
+import SwiftUI
+
+struct ShareSheet: View {
+
+    private var selectedGPSData = [GPSData]()
+    private var url: URL
+
+    init(selectedGPSData: [GPSData]) {
+        self.selectedGPSData = selectedGPSData
+        self.url = FileBuilder.output(forType: .text, selectedGPSData: selectedGPSData)!
+    }
+
+    var body: some View {
+        ActivityViewController(activityItems: [url])
+    }
+
+    func buildFile(forFileType: FileType) -> URL {
+        switch forFileType {
+//        case .waypoints:
+//            print("Building waypoint GPX file")
+//            if let documentURL = FileBuilder.output(forType: .waypoints, selectedGPSData: selectedGPSData) {
+//                //actionSheet(sharing: documentURL)
+//                self.documentURL = documentURL
+//                isSharePresented = true
+//            }
+//        case .route:
+//            print("Building route GPX file")
+//            if let documentURL = FileBuilder.output(forType: .route, selectedGPSData: selectedGPSData) {
+//                //actionSheet(sharing: documentURL)
+//                self.documentURL = documentURL
+//                isSharePresented = true
+//            }
+//        case .track:
+//            print("Building track GPX file")
+//            if let documentURL = FileBuilder.output(forType: .track, selectedGPSData: selectedGPSData) {
+//                //actionSheet(sharing: documentURL)
+//                self.documentURL = documentURL
+//                isSharePresented = true
+//            }
+        case .text:
+            print("Building plain text file")
+            if let url = FileBuilder.output(forType: .text, selectedGPSData: selectedGPSData) {
+                //actionSheet(sharing: documentURL)
+//                isSharePresented = true
+                return url
+            }
+        case .csv:
+            print("Building CSV file")
+            if let url = FileBuilder.output(forType: .csv, selectedGPSData: selectedGPSData) {
+                //actionSheet(sharing: documentURL)
+                //self.documentURL = documentURL
+//                isSharePresented = true
+                return url
+            }
+//        case .document:
+//            print("Building Rich Text file")
+//            if let documentURL = FileBuilder.output(forType: .document, selectedGPSData: selectedGPSData) {
+//                //actionSheet(sharing: documentURL)
+//                self.documentURL = documentURL
+//                isSharePresented = true
+//            }
+        default:
+            return URL(string: "microsoft.com")!
+        }
+        return URL(string: "microsoft.com")!
+    }
+
+
+}
+
+//struct ShareSheet_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ShareSheet()
+//    }
+//}
